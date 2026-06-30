3a32b2ccf4813372099e68f4a69902627d15c99e
danaokingthon <dana.okingthon@uniacc.edu>
2026-06-29 20:27:13 -0400

    AAAA

--- Assets/00_Scripts/Attack.cs
+++ Assets/00_Scripts/Attack.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    public float attackRange = 1.5f;
     public float damage = 10f;
     public Collider damageCollider;
     AnimationCharacter animationCharacter;
@@ -18,14 +19,20 @@ public class Attack : MonoBehaviour
         if (animationCharacter.isAttacking) return; // Evitar ataques consecutivos 
         animationCharacter.isAttacking = true;
         animationCharacter.animator.SetBool("Attack", animationCharacter.isAttacking);
-        Collider[]hits = Physics.OverlapBox(damageCollider.bounds.center, 
-        damageCollider.bounds.extents, damageCollider.transform.rotation);
+        Collider[] hits = Physics.OverlapSphere(transform.position, attackRange);
         foreach (Collider hit in hits)        {
             if (hit.gameObject != gameObject) // Evitar dañarse a sí mismo
             {
                 IDamageable damageable = hit.GetComponent<IDamageable>();
                 if (damageable != null)
                 {
+                    Vector3 direction = (hit.transform.position - transform.position).normalized;
+
+                    if (Vector3.Dot(transform.forward, direction) > 0.3f)
+                    {
+                        damageable.TakeDamage(damage);
+                        Debug.Log($"{hit.gameObject.name} ha recibido {damage} de daño.");
+                    }
                     damageable.TakeDamage(damage);
                     Debug.Log($"{hit.gameObject.name} ha recibido {damage} de daño.");
                 }
